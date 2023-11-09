8963c1712192d41146df4e7871b050bd97461894
Jakiur1234 <ajdmjakiurrahman1234@gmail.com>
2023-11-09 10:25:47 +0600

    Basic Start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Competitive-Programming
+Solution of some Online Judge(OJ). Some of the websites are Toph,Timus,DimmijOJ,LIghtOJ,Codeforces,Leetcode,Hackerrank,Geekforgeeks,UVa.
+Find the solution and understand the code and also help us to grow this. Upload your own solution with the exact id number of the online judges.
---
+++ template.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define D double
+#define SCI(t) scanf("%d",&t)
+#define SCLI(t) scanf("%ld",&t)
+#define SCLLI(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCD(t) scanf("%lf",&t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define FORE(i, j, k, in) for (int i=j ; i<=k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>k ; i-=in)
+#define RFORE(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define FoR(i, j) FOR(i, 0, j, 1)
+#define RFoR(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+/****** Template of some basic operations *****/
+template<typename T, typename U> inline void amin(T &x, U y) { if(y < x) x = y; }
+template<typename T, typename U> inline void amax(T &x, U y) { if(x < y) x = y; }
+/**********************************************/
+
+/******** User-defined Function *******/
+void solve(){
+
+}
+
+/**************************************/
+
+/********** Main()  function **********/
+int main()
+{
+
+	#ifndef ONLINE_JUDGE
+		//freopen("input.txt","r",stdin);
+		//freopen("output.txt","w",stdout);
+	#endif
+
+	int tc;
+	SCI(tc);
+
+	FOR(i,0,tc,1){
+			
+	}
+	return 0;
+}
