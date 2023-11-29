9c75bfc05fb653d7a98f7090ce290e40d649e416
Jakiur Rahman <70786208+Jakiur1234@users.noreply.github.com>
2023-11-30 02:48:24 +0600

    Update the template

--- template.cpp
+++ template.cpp
@@ -24,32 +24,60 @@ template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr
 #define vs vector<string>
 #define pii pair<int, int>
 #define pll pair<ll, ll>
+#define pci pair<char, int>
 #define psi pair<string, int>
 #define psl pair<string, ll>
 #define pss pair<string, string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define vpci vector<pci>
+#define vpsi vector<psi>
+#define vpsl vector<psl>
+#define vpss vector<pss>
 #define mii map<int, int>
 #define mll map<ll, ll>
+#define mci map<char, int>
 #define msi map<string, int>
 #define msl map<string, ll>
 #define umii unordered_map<int, int>
 #define umll unordered_map<ll, ll>
+#define umci unordered_map<char, int>
 #define umsi unordered_map<string, int>
 #define umsl unordered_map<string, ll>
 #define sz(x) ((int)x.size())
 #define all(a) (a).begin(), (a).end()
 #define SC(a) cin >> (a)
+#define SC2(a,b) cin >> (a) >> (b)
+#define SC3(a,b,c) cin >> (a) >> (b) >> (c)
+#define SC4(a,b,c,d) cin >> (a) >> (b) >> (c) >> (d)
 #define SIG() cin.ignore()
 #define SS(s) getline(cin, (s))
-#define fs first
-#define sc second
-#define pb push_back
-#define ins insert
 #define memzero(x) memset(x, 0, sizeof(x))
 #define mem(a, b) memset(a, b, sizeof(a))
 #define even(x) !(x & 1)
 #define odd(x) x & 1
 #define MAX(a) *max_element(all(a))
 #define MIN(a) *min_element(all(a))
+
+#define fs first
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define pob pop_back
+#define mp make_pair
+#define ins insert
+#define last back()
+#define leng length()
+
+#define br cout<<endl
+#define pf(a) cout<<a
+#define pfn(a) cout<<a<<endl
+#define pfd(d,a) cout<<setprecision(d)<<a
+#define pfdn(d,a) cout<<setprecision(d)<<a<<endl
+#define pfs(s,a) cout<<s<<a
+#define pfsn(s,a) cout<<s<<a<<endl
+#define pfsd(s,d,a) cout<<s<<setprecision(d)<<a
+#define pfsdn(s,d,a) cout<<s<<setprecision(d)<<a<<endl
 
 #define PI 3.1415926535897932384626433832795
 const int MAX_N = 1e5 + 5;
@@ -78,6 +106,7 @@ int main() {
     cout.tie(0);
     int tc = 1;
     // cin >> tc;
+    // SIG();
     for (int t = 1; t <= tc; t++) {
         // cout << "Case #" << t << ": ";
         solve();
